2cc9258e64ca70968a69f0b2ef6a52605071e5ee
fuhanxiang <FHX100279a>
2020-08-19 14:33:17 +0800

    注册登录功能修改及补全

--- src/main/java/com/demofactory/syscontrol/controller/admin/index/UserController.java
+++ src/main/java/com/demofactory/syscontrol/controller/admin/index/UserController.java
@@ -23,30 +23,35 @@ public class UserController {
     @Reference(check = false)
     private SysUserService sysUserService;
 
-    @Autowired
-    private SysUserDao sysUserDao;
     @PostMapping("login")
-    public String toLogin(String account,String password){
+    public String login(@RequestBody SysUser sysUser){
 
-        SysUser sysUser = sysUserService.loginByAccountAndPassword(account,password);
-        if (sysUser!= null){
-            LocalDateTime localDateTime = sysUserService.findLastLoginTimeByAccount(account);
-            sysUserService.updateLastLoginTime(LocalDateTime.now(),account);
+        if (sysUser.getAccount().equals("")||sysUser.getPassword().equals("")){
+            return "账号密码不能为空";
+        }
+        SysUser sysUser1 = sysUserService.login(sysUser);
+        if (sysUser1==null){
+            return "账号密码错误";
+        }
+        if (sysUser1.getStatus()==1){
+            LocalDateTime localDateTime = sysUser1.getLastLoginTime();
+            sysUserService.updateLastLoginTime(sysUser1);
             if (localDateTime == null){
                 return "欢迎您第一次登录！";
             }
             return "登录成功！上次登录时间为："+localDateTime;
         }
-        return "登录失败！";
+        return "账户已删除或停用！";
     }
     @PostMapping("register")
-    public String toRegister(String account,String password,String secondaryPwd,String pwdHint){
-        int flag = sysUserService.registerSysUser(account,password,secondaryPwd,pwdHint);
+    public String register(@RequestBody SysUser sysUser,@RequestParam String secondaryPwd){
+        if (sysUser.getPassword().equals("")||sysUser.getAccount().equals("")||secondaryPwd.equals("")){
+            return "注册信息不能为空！";
+        }
+        int flag = sysUserService.register(sysUser,secondaryPwd);
         switch (flag){
             case 1:
                 return "注册成功";
-            case 0:
-                return "账号或密码为空，请重新输入";
             case -1:
                 return "账号已存在！";
             case -2:
@@ -54,14 +59,6 @@ public class UserController {
             default:
                 return "未知错误请联系管理员";
         }
-    }
-    @GetMapping("demo")
-    public String testDemo(){
-        SysUser sysUser = new SysUser();
-        sysUser.setPassword("123");
-        sysUser.setAccount("456");
-        sysUserDao.insert(sysUser);
-        return "插入成功";
     }
 
     @PostMapping("forgetPassword")
