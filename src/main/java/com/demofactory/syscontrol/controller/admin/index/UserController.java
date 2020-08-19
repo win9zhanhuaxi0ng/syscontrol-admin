2053531e83437e5f8ff6c282bc0920268d0c83ef
fuhanxiang <FHX100279a>
2020-08-19 16:54:37 +0800

    注册功能正则表达式完善

--- src/main/java/com/demofactory/syscontrol/controller/admin/index/UserController.java
+++ src/main/java/com/demofactory/syscontrol/controller/admin/index/UserController.java
@@ -1,52 +1,81 @@
 package com.demofactory.syscontrol.controller.admin.index;
 
 import com.demofactory.syscontrol.api.SysUserService;
-import com.demofactory.syscontrol.dao.SysUserDao;
+import com.demofactory.syscontrol.controller.admin.utils.RegexUtil;
 import com.demofactory.syscontrol.domain.SysUser;
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.util.Locale;
 
+
+/**
+ * @author FHX
+ */
 @RestController
 @RequestMapping("index")
 public class UserController {
+    //手机号正则表达式
+    private static final String REGEX_MOBILE = "^1[3|4|5|7|8][0-9]{9}$";
+    //邮箱正则表达式
+    private static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+    //密码正则表达式
+    private static final String REGEX_PASSWORD = "^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).{6,}$";
 
     @Reference(check = false)
     private SysUserService sysUserService;
 
-    @Autowired
-    private SysUserDao sysUserDao;
     @PostMapping("login")
-    public String toLogin(String account,String password){
+    public String login(@RequestBody SysUser sysUser) {
 
-        SysUser sysUser = sysUserService.loginByAccountAndPassword(account,password);
-        if (sysUser!= null){
-            LocalDateTime localDateTime = sysUserService.findLastLoginTimeByAccount(account);
-            sysUserService.updateLastLoginTime(LocalDateTime.now(),account);
-            if (localDateTime == null){
+        if (sysUser.getAccount().equals("") || sysUser.getPassword().equals("")) {
+            return "账号密码不能为空";
+        }
+        SysUser sysUser1 = sysUserService.login(sysUser);
+        if (sysUser1 == null) {
+            return "账号密码错误";
+        }
+        if (sysUser1.getStatus() == 1) {
+            LocalDateTime localDateTime = sysUser1.getLastLoginTime();
+            sysUserService.updateLastLoginTime(sysUser1);
+            if (localDateTime == null) {
                 return "欢迎您第一次登录！";
             }
-            return "登录成功！上次登录时间为："+localDateTime;
+            return "登录成功！上次登录时间为：" + localDateTime;
         }
-        return "登录失败！";
+        return "账户已删除或停用！";
     }
+
     @PostMapping("register")
-    public String toRegister(String account,String password,String secondaryPwd,String pwdHint){
-        int flag = sysUserService.registerSysUser(account,password,secondaryPwd,pwdHint);
-        switch (flag){
+    public String register(@RequestBody SysUser sysUser, @RequestParam String secondaryPwd) {
+        if (sysUser.getPassword().equals("") || sysUser.getAccount().equals("") || secondaryPwd.equals("")) {
+            return "注册信息不能为空！";
+        }
+        //含有正则表达式验证方法的自定义类
+        RegexUtil regexUtil = new RegexUtil();
+        //手机正则表达式验证
+        boolean flagMobile = regexUtil.checkRegex(REGEX_MOBILE,sysUser.getAccount());
+        //邮箱正则表达式验证
+        boolean flagEmail = regexUtil.checkRegex(REGEX_EMAIL,sysUser.getAccount());
+        //密码正则表达式验证
+        boolean flagPassword = regexUtil.checkRegex(REGEX_PASSWORD,sysUser.getPassword());
+
+        if (flagEmail) {
+            sysUser.setUserEmail(sysUser.getAccount());
+        } else if (flagMobile) {
+            sysUser.setUserPhone(sysUser.getAccount());
+        } else {
+            return "账号格式不符合！";
+        }
+        if (!flagPassword){
+            return "密码格式不符合！";
+        }
+        int flag = sysUserService.register(sysUser, secondaryPwd);
+        switch (flag) {
             case 1:
                 return "注册成功";
-            case 0:
-                return "账号或密码为空，请重新输入";
             case -1:
                 return "账号已存在！";
             case -2:
@@ -55,25 +84,16 @@ public class UserController {
                 return "未知错误请联系管理员";
         }
     }
-    @GetMapping("demo")
-    public String testDemo(){
-        SysUser sysUser = new SysUser();
-        sysUser.setPassword("123");
-        sysUser.setAccount("456");
-        sysUserDao.insert(sysUser);
-        return "插入成功";
-    }
 
     @PostMapping("forgetPassword")
-    public String forgetPassword(SysUser sysUser)
-    {
-        if(StringUtils.isBlank(sysUser.getAccount())){
+    public String forgetPassword(SysUser sysUser) {
+        if (StringUtils.isBlank(sysUser.getAccount())) {
             return "账号不能为空";
         }
 
-        if(StringUtils.isBlank(sysUser.getPwdHint())){
+        if (StringUtils.isBlank(sysUser.getPwdHint())) {
             return "提示语不能为空";
         }
-        return sysUserService.SelectAccountAndHint(sysUser);
+        return sysUserService.selectAccountAndHint(sysUser);
     }
 }
